d6b123316991375cc8832a781376f5fae72a829e
Timon Christl <timonchristl@gmail.com>
2019-02-27 20:20:53 +0100

    Migrated from .NET Framework 4 to .NET Standard 1.0

--- src/TC.Colors/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("TC.Colors")]
-[assembly: AssemblyDescription("Timon Christl's color library")]
-[assembly: ComVisible(false)]
-[assembly: Guid("ad2c4565-77be-4cd7-b572-b73d1cfd8fa2")]
