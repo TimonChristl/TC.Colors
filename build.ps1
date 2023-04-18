84aab63fd850114c70a89e3d994d9f8810711f67
Timon Christl <timonchristl@gmail.com>
2023-04-18 19:44:49 +0200

    Small improvements to build modernization

--- build.ps1
+++ build.ps1
@@ -9,26 +9,28 @@
 $ErrorActionPreference="Stop"
 
 #         ↓↓↓↓↓
-$VERSION="2.0.0"
+$VERSION="2.0.1"
 
 $BUILD_NUMBER = [System.Environment]::GetEnvironmentVariable('BUILD_NUMBER')
+if([String]::IsNullOrEmpty($BUILD_NUMBER)) { $BUILD_NUMBER=999 }
 $FULL_VERSION = "$VERSION.$BUILD_NUMBER"
 
 $NUGET_SOURCE="gitea NuGet"
 $GITEA_API_KEY = [System.Environment]::GetEnvironmentVariable('GITEA_API_KEY')
+if([String]::IsNullOrEmpty($GITEA_API_KEY)) { Write-Output "Environment variable GITEA_API_KEY not set or empty"; exit 1 }
 
 Push-Location src
 try {
-    dotnet clean /p:Version=$FULL_VERSION
+    dotnet clean
     if(!$?) { exit 1 }
 
-    dotnet restore /p:Version=$FULL_VERSION
+    dotnet restore
     if(!$?) { exit 1 }
 
     dotnet build /p:Configuration=Release /p:Version=$FULL_VERSION
     if(!$?) { exit 1 }
 
-    dotnet pack /p:Configuration=Release /p:Version=$FULL_VERSION
+    dotnet pack /p:Configuration=Release /p:Version=$FULL_VERSION /p:PackageVersion=$VERSION
     if(!$?) { exit 1 }
 
     dotnet nuget push --source $NUGET_SOURCE --api-key=$GITEA_API_KEY "TC.Colors\bin\Release\TC.Colors.$FULL_VERSION.nupkg"
